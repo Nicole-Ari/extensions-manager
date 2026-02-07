10886ee8ef0e863fdad81515402b57320473166f
Nicole-Ari <nicole.mamiarisoa@gmail.com>
2026-02-07 14:00:28 +0300

    Fix: empty state UI

--- src/components/SelectedExtensions.jsx
+++ src/components/SelectedExtensions.jsx
@@ -77,80 +77,85 @@ function SelectedExtensions() {
           </button>
         </div>
       </div>
-      {loading && <div className="result">Loading ...</div>}
-      {!loading && activeList && activeExtList.length === 0 && (
-        <div className="result">No extensions are currently active</div>
+      {loading ? (
+        <div className="result">Loading ...</div>
+      ) : (
+        <>
+          {!loading && activeList && activeExtList.length === 0 && (
+            <div className="result">No extensions are currently active</div>
+          )}
+          {!loading && inactiveList && inactiveExtList.length === 0 && (
+            <div className="result">No disabled extensions</div>
+          )}
+          {!loading &&
+            !inactiveList &&
+            !activeList &&
+            selectedList.length === 0 && (
+              <div className="result">No extensions added yet</div>
+            )}
+
+          <div className="cards-container">
+            {activeList &&
+              activeExtList.length !== 0 &&
+              activeExtList
+                ?.filter((el) => el.active === true)
+                .map((el, index) => {
+                  const checkboxId = `state-${el.name}`;
+                  return (
+                    <Card
+                      data={el}
+                      key={el.name}
+                      checkboxId={checkboxId}
+                      list={list}
+                      selectedList={selectedList}
+                      handleSelectedList={setSelectedList}
+                      handleList={setList}
+                      setPrevlist={setPrevlist}
+                    />
+                  );
+                })}
+
+            {inactiveList &&
+              inactiveExtList.length !== 0 &&
+              inactiveExtList
+                ?.filter((el) => el.active === false)
+                .map((el, index) => {
+                  const checkboxId = `state-${el.name}`;
+                  return (
+                    <Card
+                      data={el}
+                      key={el.name}
+                      checkboxId={checkboxId}
+                      list={list}
+                      selectedList={selectedList}
+                      handleSelectedList={setSelectedList}
+                      handleList={setList}
+                      setPrevlist={setPrevlist}
+                    />
+                  );
+                })}
+
+            {!inactiveList &&
+              !activeList &&
+              selectedList.length !== 0 &&
+              selectedList.map((el, index) => {
+                const checkboxId = `state-${el.name}`;
+                return (
+                  <Card
+                    data={el}
+                    key={index}
+                    checkboxId={checkboxId}
+                    list={list}
+                    selectedList={selectedList}
+                    handleSelectedList={setSelectedList}
+                    handleList={setList}
+                    setPrevlist={setPrevlist}
+                  />
+                );
+              })}
+          </div>
+        </>
       )}
-      {!loading && inactiveList && inactiveExtList.length === 0 && (
-        <div className="result">No disabled extensions</div>
-      )}
-      {!loading &&
-        !inactiveList &&
-        !activeList &&
-        selectedList.length === 0 && (
-          <div className="result">No extensions added yet</div>
-        )}
-
-      <div className="cards-container">
-        {activeList &&
-          activeExtList.length !== 0 &&
-          activeExtList
-            ?.filter((el) => el.active === true)
-            .map((el, index) => {
-              const checkboxId = `state-${el.name}`;
-              return (
-                <Card
-                  data={el}
-                  key={el.name}
-                  checkboxId={checkboxId}
-                  list={list}
-                  selectedList={selectedList}
-                  handleSelectedList={setSelectedList}
-                  handleList={setList}
-                  setPrevlist={setPrevlist}
-                />
-              );
-            })}
-
-        {inactiveList &&
-          inactiveExtList.length !== 0 &&
-          inactiveExtList
-            ?.filter((el) => el.active === false)
-            .map((el, index) => {
-              const checkboxId = `state-${el.name}`;
-              return (
-                <Card
-                  data={el}
-                  key={el.name}
-                  checkboxId={checkboxId}
-                  list={list}
-                  selectedList={selectedList}
-                  handleSelectedList={setSelectedList}
-                  handleList={setList}
-                  setPrevlist={setPrevlist}
-                />
-              );
-            })}
-
-        {!inactiveList &&
-          !activeList &&
-          selectedList.length !== 0 &&
-          selectedList.map((el, index) => {
-            const checkboxId = `state-${el.name}`;
-            return (
-              <Card
-                data={el}
-                key={index}
-                checkboxId={checkboxId}
-                list={list}
-                selectedList={selectedList}
-                handleSelectedList={setSelectedList}
-                handleList={setList}
-                setPrevlist={setPrevlist}
-              />
-            );
-          })}
-      </div>
     </div>
   );
 }
