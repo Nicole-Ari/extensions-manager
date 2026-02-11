ef16a062c4644ae4fd562634006983068f3bb257
Nicole-Ari <nicole.mamiarisoa@gmail.com>
2026-02-12 01:06:39 +0300

    Fix: Add extension

--- src/components/AddExtension.jsx
+++ src/components/AddExtension.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "./Card";
 import "./addExtension.css";
 import { useNavigate, useOutletContext } from "react-router-dom";
